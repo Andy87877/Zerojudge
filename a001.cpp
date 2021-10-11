80585c23b75f8bee175106a3a5e416481d4c30e8
Andy8787 <86054592+Andy87877@users.noreply.github.com>
2021-10-12 01:06:32 +0800

    Create a001.cpp

---
+++ a001.cpp
@@ -0,0 +1,11 @@
+// https://replit.com/@andy8787main/a001-c#main.cpp
+// https://zerojudge.tw/ShowProblem?problemid=a001
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    cout << "hello, " << s;
+}
