84e642ccc47fab59e91410c755e5e8d653d0bc35
Andy8787 <86054592+Andy87877@users.noreply.github.com>
2021-10-12 01:14:36 +0800

    Create a001.c

---
+++ a001.c
@@ -0,0 +1,12 @@
+// https://replit.com/@andy8787main/a001-c-1#main.c
+// https://zerojudge.tw/ShowProblem?problemid=a001
+
+#include <stdio.h>
+
+int main(void) {
+    char str[80]; 
+    scanf("%s", str);
+    printf("hello, %s", str);
+    return 0;
+}
+// C我比較不熟
