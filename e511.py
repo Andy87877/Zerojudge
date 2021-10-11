3556ba2091f7e2284c2035eb96f966799c826312
Andy8787 <86054592+Andy87877@users.noreply.github.com>
2021-10-12 01:02:28 +0800

    Create e511.py

---
+++ e511.py
@@ -0,0 +1,9 @@
+# https://replit.com/@andy8787main/e511#main.py
+# https://zerojudge.tw/ShowProblem?problemid=e511
+
+times = eval(input())
+for timess in range(times):
+    n = eval(input())
+    l = [int(i) for i in input().split(' ')]
+    ans = (((max(l))-(min(l)-1))*2)-2; #好像是還要回到停車位的路程
+    print(ans)
