a6afa4559656820061c758dd8cef449962a79025
Andy8787 <86054592+Andy87877@users.noreply.github.com>
2021-11-25 00:42:38 +0800

    Create d498.cpp

---
+++ d498.cpp
@@ -0,0 +1,12 @@
+// https://replit.com/@andy8787main/d498-cpp#main.cpp
+// https://zerojudge.tw/ShowProblem?problemid=d498
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << "I don't say swear words!\n";
+    }
+} 
