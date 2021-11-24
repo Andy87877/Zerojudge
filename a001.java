9c2e6c4563cb33c40f2aca460dd2fbf2f9d6f8d3
Andy8787 <86054592+Andy87877@users.noreply.github.com>
2021-11-24 14:15:10 +0800

    Create a001.java

---
+++ a001.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        String str = cin.nextLine();
+        System.out.println("hello, "+str);
+    }
+}
