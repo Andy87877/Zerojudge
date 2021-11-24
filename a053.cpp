4589c5dd974bd051cd58b7a67c2407d62e95dca7
Andy8787 <86054592+Andy87877@users.noreply.github.com>
2021-11-24 15:18:29 +0800

    Create a053.cpp

---
+++ a053.cpp
@@ -0,0 +1,23 @@
+// https://replit.com/@andy8787main/a053-cpp-1#main.cpp
+// https://zerojudge.tw/ShowProblem?problemid=a053
+#include <iostream>
+using namespace std;
+
+int judge(int num) {
+    if (num >= 40) {
+        return 100;
+    } else if (num > 20) {
+        return 80+num-20;
+    } else if (num > 10) {
+        return 60+(num-10)*2;
+    } else {
+        return num*6;
+    }
+}
+
+int main() {
+    int num;
+    while(cin >> num) {
+        cout << judge(num) << endl;
+    }
+} 
