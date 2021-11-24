c7a74676121c890f40a3a6d46bca26873eadf213
Andy8787 <86054592+Andy87877@users.noreply.github.com>
2021-11-24 14:30:20 +0800

    Create a002.java

---
+++ a002.java
@@ -0,0 +1,11 @@
+// https://replit.com/@andy8787main/a002-java#Main.java
+// https://zerojudge.tw/ShowProblem?problemid=a002
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int a = cin.nextInt();
+        int b = cin.nextInt();
+        System.out.println(a+b);
+    }
+}
