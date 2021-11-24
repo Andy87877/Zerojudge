7bc5efb654469f6700ecdff483997a5ce71f8737
Andy8787 <86054592+Andy87877@users.noreply.github.com>
2021-11-24 14:18:02 +0800

    Update a001.java

--- a001.java
+++ a001.java
@@ -1,8 +1,10 @@
-import java.util.Scanner;
-class Main {
+// https://replit.com/@andy8787main/a001-java#Main.java
+// https://zerojudge.tw/ShowProblem?problemid=a001
+import java.util.Scanner; // 輸入要用
+class Main { // main
     public static void main(String[] args) {
-        Scanner cin = new Scanner(System.in);
-        String str = cin.nextLine();
-        System.out.println("hello, "+str);
+        Scanner cin = new Scanner(System.in); // cin
+        String str = cin.nextLine(); // cin
+        System.out.println("hello, "+str); // cout
     }
 }
