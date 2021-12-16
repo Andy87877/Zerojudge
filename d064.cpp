7d4fd0b9523c118e0aac123481e11bf971ecab2d
Andy8787(布長) <86054592+Andy87877@users.noreply.github.com>
2021-12-16 19:49:48 +0800

    Create d064.cpp

---
+++ d064.cpp
@@ -0,0 +1,14 @@
+//https://zerojudge.tw/ShowProblem?problemid=d064
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+    if ((num % 2) == 0) {
+        cout << "Even";
+    } else {
+        cout << "Odd";
+    }
+}
