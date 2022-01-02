075a45302aff2aec7f544b08a691f0260d6e25cf
Andy8787(布長) <86054592+Andy87877@users.noreply.github.com>
2022-01-02 19:53:47 +0800

    Update d072.cpp

--- d072.cpp
+++ d072.cpp
@@ -1,10 +1,13 @@
 // https://replit.com/@andy8787main/d072-cpp#main.cpp
 // https://zerojudge.tw/ShowProblem?problemid=d072
+
 #include <iostream>
 using namespace std;
+
 int main() {
     int times;
     cin >> times;
+    
     for (int i = 0; i < times; i++) {
         int year;
         cin >> year;
