e9c574fe879644d946ca61c020d6b47d44052ed6
Andy8787(布長) <86054592+Andy87877@users.noreply.github.com>
2022-01-14 00:03:46 +0800

    Update c031.cpp

--- c031.cpp
+++ c031.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int num;
+    while(cin >> num) {
+        int a=1, b=1, max=1;
+        bool flag = false; //true 右上到左下
+        //a.b 現在
+        //c.d 最多
+        for (int i = 1; i < num; i++) {
+            if (flag) {
+                if (b >= max) {
+                    flag = false;
+                    max++;               
+                    a = 2;
+                }
+                a--;
+                b++;
+            } else {
+                if (a >= max) {
+                    flag = true;
+                    max++;
+                    b = 2;                
+                }
+                b--;
+                a++;
+            }
+        }
+        cout << "TERM " << num << " IS ";
+        cout << b << '/' << a << endl;
+    }
+}
+/*
+1 2 6 7
+3 5 8
+4 9
+10
+*/
