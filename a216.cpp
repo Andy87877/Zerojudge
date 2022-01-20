a0b8754736cf5e25d1f3d4d06bbdd71be6c2caf3
Andy8787(布長) <86054592+Andy87877@users.noreply.github.com>
2022-01-20 17:31:30 +0800

    Update a216.cpp

--- a216.cpp
+++ a216.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <vector>
+#define int long long
+using namespace std;
 
+signed main() {
+    int num;
+
+    while(cin >> num) {
+        vector<int> f;
+        vector<int> g;
+
+        f.push_back(1);
+        g.push_back(1);
+
+        for (int i = 2; i <= num; i++) {
+            f.push_back(f[i-2]+i);
+        }
+        for (int i = 2; i <= num; i++) {
+            g.push_back(g[i-2]+f[i-1]);
+        }
+        cout << f[num-1] << ' ' << g[num-1] << endl;
+    }
+}
