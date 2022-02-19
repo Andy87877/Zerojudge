e36a64c0fb787fcc198fc28f987cafd110a75029
Andy8787(布長) <86054592+Andy87877@users.noreply.github.com>
2022-02-20 01:01:35 +0800

    Create d067.cpp

---
+++ d067.cpp
@@ -0,0 +1,17 @@
+// https://zerojudge.tw/ShowProblem?problemid=d067
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if ((n % 400) == 0) {
+        cout << "a leap year";
+    } else {
+        if (((n % 4) == 0) && ((n % 100) != 0)) {
+            cout << "a leap year";
+        } else {
+            cout << "a normal year";
+        }
+    }
+}
