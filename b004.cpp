37d21f73361ae7fb9df7388825791718b0ad296d
Andy8787(布長) <86054592+Andy87877@users.noreply.github.com>
2022-03-01 23:14:24 +0800

    Update b004.cpp

--- b004.cpp
+++ b004.cpp
@@ -4,10 +4,11 @@
 using namespace std;
 
 int main() {
-    double D,L,pi,area;
-    while(cin >> D >>L) {   
-        pi = 2*acos(0);
-        area = pi*L/2*sqrt( (L/2)*(L/2) -(D/2)*(D/2) );
-        printf("%.3f\n",area);
+    double D, L, pi, ans;
+    pi = 2*acos(0);
+    
+    while(cin >> D >> L) {
+        ans = pi*(L/2)*sqrt((L/2)*(L/2)-(D/2)*(D/2));
+        printf("%.3f\n",(ans));
     }
 }
