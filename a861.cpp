5277b14cbcfeb8ca86e011975cbb333de7a50af3
Andy8787(布長) <86054592+Andy87877@users.noreply.github.com>
2022-03-04 01:02:40 +0800

    Create a861.cpp

---
+++ a861.cpp
@@ -0,0 +1,11 @@
+// https://zerojudge.tw/ShowProblem?problemid=a861
+#include <iostream>
+#define int long long
+using namespace std;
+
+signed main() {
+    int H, W;
+    while(cin >> H >> W) {
+        cout << (2*H+2*W) << endl;
+    }
+}
