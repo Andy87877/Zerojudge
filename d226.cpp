1d3c4f8e76dc75d247607fa58cd9e6d814ff4619
Andy8787(布長) <86054592+Andy87877@users.noreply.github.com>
2022-03-04 01:28:17 +0800

    Create d226.cpp

---
+++ d226.cpp
@@ -0,0 +1,10 @@
+// https://zerojudge.tw/ShowProblem?problemid=d226
+#include <iostream>
+using namespace std;
+
+int main() {
+    int v, t;
+    while (cin >> v >> t) {
+        cout << v*t*2 << endl;
+    }
+}
