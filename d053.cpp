cb94188704633b373f78f1a880f242d59e2fa17a
Andy8787(布長) <86054592+Andy87877@users.noreply.github.com>
2022-03-04 22:18:20 +0800

    Create d053.cpp

---
+++ d053.cpp
@@ -0,0 +1,11 @@
+// https://zerojudge.tw/ShowProblem?problemid=d053
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n, m;
+    // 帶數字:(
+    while (cin >> n >> m) {  
+        cout << n*m-1 << endl;
+    }
+}
