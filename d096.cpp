7c026a653a451f188d2e7d8b63f0191fa4e68452
Andy8787(布長) <86054592+Andy87877@users.noreply.github.com>
2022-03-09 21:54:33 +0800

    Update d096.cpp

--- d096.cpp
+++ d096.cpp
@@ -1,1 +1,11 @@
+// https://zerojudge.tw/ShowProblem?problemid=d096
+#include <iostream>
+using namespace std;
 
+int main() {
+    long long n;
+    while (cin >> n) {
+        cout << (((3*n*n+6*n+3)/2)-9) << endl;
+    }
+}
+// https://yungshenglu.github.io/2018/06/01/UVA913/
