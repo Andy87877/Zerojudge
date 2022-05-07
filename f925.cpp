702a2386e6cca027e4a664f34a0ec24bc5ddd657
Andy8787(布長) <86054592+Andy87877@users.noreply.github.com>
2022-05-07 16:47:08 +0800

    Create f925.cpp

---
+++ f925.cpp
@@ -0,0 +1,13 @@
+// https://zerojudge.tw/Submissions
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m,d;
+    cin >> m >> d;
+    if (m == 6 && d == 12) {
+        cout << "Yes";
+    } else {
+        cout << "No";
+    }
+}
