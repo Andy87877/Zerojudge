0a5138fb61007aba559007ffdd7529be8f27a6eb
Andy8787(布長) <andy8787main@gmail.com>
2022-05-30 01:16:01 +0800

    條件判斷與基礎邏輯

---
+++ f312.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+// #include <cmath>
+// #include <map>
+// #include <string>
+// #include <algorithm>
+#define ll long long
+using namespace std;
+
+int main() {
+    ll a,b,c;
+    ll d,e,f;
+    ll n;
+    cin >> a >> b >> c;
+    cin >> d >> e >> f;
+    cin >> n;
+    ll ans = (c + d*(n)*(n)+e*(n)+f), temp;
+    for (int i = 1; i <= n; i++) {
+        temp = a*i*i+b*i+c;
+        temp += d*(n-i)*(n-i)+e*(n-i)+f;
+        if (temp > ans) {
+            ans = temp;
+        }
+    }
+    cout << ans << endl;
+}
