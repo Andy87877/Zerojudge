0505c10cbb0d332d4114f59c5d06a421c8c0e9d3
Andy8787(布長) <andy8787main@gmail.com>
2022-08-16 22:17:04 +0800

    a130

---
+++ a130.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void solve() {
+	string str[10];
+	ll num[10] , mx = -1;
+	for (ll i = 0; i < 10; i++) {
+		cin >> str[i] >> num[i];
+		if (mx < num[i]) mx = num[i];
+	}
+	
+	for (ll i = 0; i < 10; i++) {
+		if (mx == num[i]) {
+			cout << str[i] << "\n";
+		}
+	}
+}
+
+int main() {
+	ll t;
+	cin >> t;
+	for (ll i = 1; i <= t; i++) {
+		cout << "Case #" << i << ":\n";
+		solve();
+	}
+} 
