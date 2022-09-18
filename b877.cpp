6538f0d0e10a6843a10337e738d726a0cab74d6b
Andy8787(布長) <andy8787main@gmail.com>
2022-09-18 19:30:29 +0800

    水題

---
+++ b877.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define Andy8787_want_AC ios::sync_with_stdio(0),cin.tie(0);cout.tie(0);
+using namespace std;
+
+int main() {
+    Andy8787_want_AC
+    ll a,b;
+    cin >> a >> b;
+    cout << (a > b)*(100-(a-b)) + (a <= b)*(b-a) << endl;
+}
